5a2225c44325bd7f5b0074922b76079661337ac0
Ruben Taelman <ruben.taelman@ugent.be>
2019-05-14 07:49:50 +0200

    Change singularization to enable better merging of values

--- lib/Converter.ts
+++ lib/Converter.ts
@@ -26,26 +26,24 @@ export class Converter {
    * @param {string} lastKeyPath The accumulated key path (separated by the given delimiter)
    *                             through recursive calls, can be empty.
    * @param {ISchema} schema A schema.
-   * @param {boolean} materializeRdfJsTerms If RDFJS terms should be materialized.
    * @param {string} delimiter The string to join key paths by.
    */
   public static addValueToTree(tree: any, path: string[], value: RDF.Term, lastKeyPath: string,
-                               schema: ISchema, materializeRdfJsTerms: boolean, delimiter: string) {
+                               schema: ISchema, delimiter: string) {
     const key = path[0];
     const keyPath = lastKeyPath ? (lastKeyPath + delimiter + key) : key;
     const singularize: boolean = schema.singularizeVariables[keyPath];
     if (path.length === 1) {
       // Leaf nodes
-      const setValue = materializeRdfJsTerms ? value.value : value;
       if (singularize) {
         if (!tree[key]) {
-          tree[key] = setValue;
+          tree[key] = value;
         }
       } else {
         if (!tree[key]) {
           tree[key] = [];
         }
-        tree[key].push(setValue);
+        tree[key].push(value);
       }
     } else {
       // Inner nodes
@@ -57,12 +55,100 @@ export class Converter {
         nextNode = tree[key];
       } else {
         if (!tree[key]) {
-          tree[key] = [];
+          tree[key] = [{}];
         }
-        nextNode = {};
-        tree[key].push(nextNode);
+        nextNode = tree[key][0];
       }
-      Converter.addValueToTree(nextNode, path.slice(1), value, keyPath, schema, materializeRdfJsTerms, delimiter);
+      Converter.addValueToTree(nextNode, path.slice(1), value, keyPath, schema, delimiter);
+    }
+  }
+
+  /**
+   * Recursively merge the two given trees.
+   * @param tree1 A first tree (has key priority on literals).
+   * @param tree2 A second tree. All arrays will/should only have a single element.
+   * @return {any} The merged tree.
+   */
+  public static mergeTrees(tree1: any, tree2: any): IMergeResult {
+    if (typeof tree1 !== typeof tree2) {
+      throw new Error(`Two incompatible tree nodes were found: ${typeof tree1} and ${typeof tree2}`);
+    }
+    if (Array.isArray(tree1) !== Array.isArray(tree2)) {
+      throw new Error(`Two incompatible tree nodes were found: Array?${Array.isArray(tree1)} and Array?${
+        Array.isArray(tree2)}`);
+    }
+
+    if (typeof tree1 === 'object' && typeof tree2 === 'object') {
+      if (tree1.termType && tree2.termType) {
+        if (tree1.equals(tree2)) {
+          return { valid: true, result: tree1 };
+        } else {
+          return { valid: false, result: tree1 };
+        }
+      }
+
+      if (Array.isArray(tree1) && Array.isArray(tree2)) {
+        if (tree1.length > 0) {
+          const merged = [];
+          let valid = false;
+          for (let i = 0; i < tree1.length; i++) {
+            const mergedElement = Converter.mergeTrees(tree1[i], tree2[0]);
+            if (mergedElement.valid) {
+              valid = true;
+              merged.push(mergedElement.result);
+            } else {
+              merged.push(tree1[i]);
+            }
+          }
+
+          if (valid) {
+            return { valid: true, result: merged };
+          }
+        }
+        return { valid: true, result: tree1.concat(tree2) };
+      } else {
+        const merged: any = {};
+
+        for (const key2 in tree2) {
+          merged[key2] = tree2[key2];
+        }
+        for (const key1 in tree1) {
+          if (merged[key1]) {
+            const mergedElement = Converter.mergeTrees(tree1[key1], merged[key1]);
+            if (mergedElement.valid) {
+              merged[key1] = mergedElement.result;
+            } else {
+              return { valid: false, result: tree1 };
+            }
+          } else {
+            merged[key1] = tree1[key1];
+          }
+        }
+
+        return { valid: true, result: merged };
+      }
+    } else {
+      throw new Error(`Unmergable tree types: ${typeof tree1} and ${typeof tree2}`);
+    }
+  }
+
+  /**
+   * Materialize all RDF terms in the given tree to raw values.
+   * This does not mutate the original tree.
+   * @param tree A tree.
+   * @return {any} A materialized tree.
+   */
+  public static materializeTree(tree: any): any {
+    if (tree.termType) {
+      return tree.value;
+    } else if (Array.isArray(tree)) {
+      return tree.map(Converter.materializeTree);
+    } else {
+      const materialized: any = {};
+      for (const key in tree) {
+        materialized[key] = Converter.materializeTree(tree[key]);
+      }
+      return materialized;
     }
   }
 
@@ -84,19 +170,23 @@ export class Converter {
    */
   public bindingsToTree(bindingsArray: IBindings[], schema: ISchema): any {
     const singularRoot = schema && schema.singularizeVariables[''];
-    const tree: any = singularRoot ? {} : [];
+    let tree: any = singularRoot ? {} : [];
     for (const bindings of bindingsArray) {
-      let subTree = tree;
-      if (!singularRoot) {
-        subTree = {};
-        tree.push(subTree);
-      }
+      const subTree: any = singularRoot ? {} : [{}];
+
       for (const key in bindings) {
         const path: string[] = key.split(this.delimiter);
         const value: RDF.Term = bindings[key];
-        Converter.addValueToTree(subTree, path, value, '', schema, this.materializeRdfJsTerms, this.delimiter);
+        Converter.addValueToTree(singularRoot ? subTree : subTree[0], path, value, '', schema, this.delimiter);
       }
+
+      tree = Converter.mergeTrees(tree, subTree).result;
     }
+
+    if (this.materializeRdfJsTerms) {
+      tree = Converter.materializeTree(tree);
+    }
+
     return tree;
   }
 
@@ -125,4 +215,16 @@ export interface ISchema {
    * Defaults to false (arrays), due to the open-world-assumption in RDF.
    */
   singularizeVariables: {[id: string]: boolean};
+}
+
+export interface IMergeResult {
+  /**
+   * If the merging was successful.
+   * If not, information was withheld from the result.
+   */
+  valid: boolean;
+  /**
+   * The result of the merge.
+   */
+  result: any;
 }
